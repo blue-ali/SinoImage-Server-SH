4b7f3f58cf3e6d18412d606957a0868675eb1c65
blue-ali <bluefcxt@hotmail.com>
2016-04-27 21:35:12 +0800

    batch表增加version字段 file表增加category字段

--- src/main/java/cn/net/sinodata/cm/hibernate/db.sql
+++ src/main/java/cn/net/sinodata/cm/hibernate/db.sql
@@ -1,5 +1,5 @@
 DROP TABLE cm_file_info;
-CREATE TABLE cm_file_info ( fileid varchar(20) NOT NULL, batchid varchar(50) NOT NULL, filename varchar(50), MD5 varchar(100), createtime timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL, creator varchar(20), PRIMARY KEY (fileid, batchid) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE cm_file_info ( fileid varchar(20) NOT NULL, batchid varchar(50) NOT NULL, filename varchar(50), MD5 varchar(100), createtime timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL, creator varchar(20), category varchar(2), PRIMARY KEY (fileid, batchid) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE cm_batch_info;
-CREATE TABLE cm_batch_info ( batchid varchar(50) NOT NULL, sysid varchar(20), orgid varchar(20), createtime timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL, lastmodified timestamp DEFAULT '0000-00-00 00:00:00' NULL, files varchar(20), creator varchar(20), version varchar(10) DEFAULT '""', provincecode varchar(20), PRIMARY KEY (batchid) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE cm_batch_info ( batchid varchar(50) NOT NULL, sysid varchar(20), orgid varchar(20), createtime timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL, lastmodified timestamp DEFAULT '0000-00-00 00:00:00' NULL, creator varchar(20), version varchar(10) DEFAULT '""', provincecode varchar(20), PRIMARY KEY (batchid) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
